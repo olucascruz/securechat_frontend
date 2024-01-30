6992ef4b2ce1790430c392ed77e9f5d6bb962435
LucasCruz <lca.snf21@uea.edu.br>
2024-01-30 18:27:21 -0400

    updateCripto

--- src/service/keys_service.js
+++ src/service/keys_service.js
@@ -27,32 +27,3 @@ export async function deriveSharedKey(privateKey, otherPublicKey) {
   );
 }
 
-// Função para criptografar a mensagem
-export async function encryptMessage(sharedKey, message) {
-  const iv = crypto.getRandomValues(new Uint8Array(16));
-
-  const encryptedMessage = await crypto.subtle.encrypt(
-    {
-      name: "AES-GCM",
-      iv: iv,
-    },
-    sharedKey,
-    new TextEncoder().encode(message)
-  );
-
-  return { iv, encryptedMessage };
-}
-
-// Função para descriptografar a mensagem
-export async function decryptMessage(sharedKey, iv, encryptedMessage) {
-  const decryptedMessage = await crypto.subtle.decrypt(
-    {
-      name: "AES-GCM",
-      iv: iv,
-    },
-    sharedKey,
-    encryptedMessage
-  );
-
-  return new TextDecoder().decode(decryptedMessage);
-}
