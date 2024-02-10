7d7a67aa107e0f51a14884ec80c434c2631cada3
LucasCruz <lca.snf21@uea.edu.br>
2024-02-10 08:58:28 -0400

    feat: add get is_online function

--- src/service/user_service.js
+++ src/service/user_service.js
@@ -65,3 +65,35 @@ export const getPublicKey = async (user_id) =>{
   }
 }
 
+export const getIsOnline = async (user_id) =>{
+  const getIsOnlineEndpoint = `http://127.0.0.1:5000/getIsOnline?user_id=${user_id}`;
+  try {
+    const response = await axios.get(getIsOnlineEndpoint);
+    // Retornando os dados da resposta
+    return response.data;
+  } catch (error) {
+      // Lidando com erros
+      console.error('Erro na requisição:', error);
+      return []; // Retorna um array vazio em caso de erro, ou você pode lidar com o erro de outra forma
+  }
+
+}
+
+
+export const logoutUser = async (userId) =>{
+  const data = {
+    "id":userId
+  };
+
+  const registerEndpoint = 'http://127.0.0.1:5000/logout';
+  try{
+    const response = await axios.post(registerEndpoint, data)
+    return response.status
+  }catch(error) {
+  // Lidando com erros
+    console.error('Erro na requisição:', error);
+    return null
+  };
+
+}
+
